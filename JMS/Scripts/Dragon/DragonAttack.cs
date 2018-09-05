18036cf481acc3082c3e93a186e2046f5cf43f60
JeonMinSu <session_min@naver.com>
2018-09-05 09:57:23 +0900

    커밋

---
+++ JMS/Scripts/Dragon/DragonAttack.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DragonController
+{ 
+    public class DragonAttack : MonoBehaviour
+    {
+        [SerializeField]
+        private float _damage;
+        public float Damage { get { return _damage; } }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (BlackBoard.Instance.IsGroundAttacking)
+            {
+                if (other.tag == "Player")
+                {
+
+                    Debug.Log("HitHitHIt");
+                    //UtilityManager.Instance.AttackPlayer(_damage);
+                }
+            }
+        }
+
+    }
+
+}
